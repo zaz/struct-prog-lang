69a321ad48b7ba10cb47635a2c08d9841aa23e58
Greg DeLozier <gregdelozier@gmail.com>
2024-03-04 16:38:02 -0500

    added logical expressions

--- topic-03-booleans/parser.py
+++ topic-03-booleans/parser.py
@@ -3,10 +3,10 @@ statement = assignment | expression
 assignment = identifier "=" expression
 arithmetic_expression = term { ("+" | "-") term }
 relational_expression = arithmetic_expression { ("<" | ">" | "<=" | ">=" | "==" | "!=") arithmetic_expression}
-TBD: logical_expression = logical_term { "||" logical_term }
-TBD: logical_term = logical_factor {"&&" logical_factor}
-TBD: logical_factor = relational_expression | "!" logical_factor
-TBD: expression = logical_expression
+logical_expression = logical_term { "||" logical_term }
+logical_term = logical_factor {"&&" logical_factor}
+logical_factor = relational_expression | "!" logical_factor
+expression = logical_expression
 term = factor { ("*" | "/") factor }
 factor = number | identifier | "(" expression ")" | "-" factor | "!" factor
 number = <number>
@@ -22,7 +22,7 @@ def parse_statement(tokens):
     if tokens[0]["tag"] == "identifier":
         if tokens[1]["tag"] == "=":
             return parse_assignment(tokens)
-    return parse_relational_expression(tokens)
+    return parse_logical_expression(tokens)
 
 
 def parse_assignment(tokens):
@@ -32,7 +32,7 @@ def parse_assignment(tokens):
     tokens = tokens[1:]
     if tokens[0]["tag"] != "=":
         raise Exception(f"Expected '=': {tokens[0]}")
-    expression, tokens = parse_relational_expression(tokens[1:])
+    expression, tokens = parse_logical_expression(tokens[1:])
     return create_node("=", left=identifier, right=expression), tokens
 
 
@@ -78,10 +78,36 @@ def parse_factor(tokens):
     if tag == "-":
         node, tokens = parse_factor(tokens[1:])
         return create_node("negate", left=node), tokens
-    if tag == "!":
-        node, tokens = parse_factor(tokens[1:])
-        return create_node("not", left=node), tokens
     raise Exception(f"Unexpected token: {tokens[0]}")
+
+
+def parse_logical_expression(tokens):
+    # logical_expression = logical_term { "||" logical_term }
+    node, tokens = parse_logical_term(tokens)
+    while tokens[0]["tag"] == "||":
+        tag = tokens[0]["tag"]
+        right_node, tokens = parse_logical_term(tokens[1:])
+        node = create_node(tag, left=node, right=right_node)
+    return node, tokens
+
+
+def parse_logical_term(tokens):
+    # logical_term = logical_factor {"&&" logical_factor}
+    node, tokens = parse_logical_factor(tokens)
+    while tokens[0]["tag"] == "&&":
+        tag = tokens[0]["tag"]
+        right_node, tokens = parse_logical_factor(tokens[1:])
+        node = create_node(tag, left=node, right=right_node)
+    return node, tokens
+
+
+def parse_logical_factor(tokens):
+    # logical_factor = relational_expression | "!" logical_factor
+    token = tokens[0]
+    if token["tag"] == "!":
+        node, tokens = parse_logical_factor(tokens[1:])
+        return create_node("not", left=node), tokens
+    return parse_relational_expression(tokens)
 
 
 def parse(tokens):
@@ -126,6 +152,72 @@ def test_simple_identifier_parsing():
         "value": None,
         "left": {"tag": "identifier", "value": "x", "left": None, "right": None},
         "right": {"tag": "identifier", "value": "y", "left": None, "right": None},
+    }
+
+
+def test_logical_operators_parsing():
+    print("test logical operators parsing")
+    for op in ["==", "!=", "||", "&&"]:
+        tokens = tokenize(f"1{op}0")
+        ast = parse(tokens)
+        assert ast == {
+            "tag": op,
+            "value": None,
+            "left": {"tag": "number", "value": 1, "left": None, "right": None},
+            "right": {"tag": "number", "value": 0, "left": None, "right": None},
+        }, f"AST for op {op} got ast = {ast}"
+    tokens = tokenize(f"1 || 0 && 1")
+    ast = parse(tokens)
+    assert ast == {
+        "tag": "||",
+        "value": None,
+        "left": {"tag": "number", "value": 1, "left": None, "right": None},
+        "right": {
+            "tag": "&&",
+            "value": None,
+            "left": {"tag": "number", "value": 0, "left": None, "right": None},
+            "right": {"tag": "number", "value": 1, "left": None, "right": None},
+        },
+    }
+    tokens = tokenize(f"1 && 0 || 1")
+    ast = parse(tokens)
+    assert ast == {
+        "tag": "||",
+        "value": None,
+        "left": {
+            "tag": "&&",
+            "value": None,
+            "left": {"tag": "number", "value": 1, "left": None, "right": None},
+            "right": {"tag": "number", "value": 0, "left": None, "right": None},
+        },
+        "right": {"tag": "number", "value": 1, "left": None, "right": None},
+    }
+    tokens = tokenize(f"1 || 0 || 0")
+    ast = parse(tokens)
+    assert ast == {
+        "tag": "||",
+        "value": None,
+        "left": {
+            "tag": "||",
+            "value": None,
+            "left": {"tag": "number", "value": 1, "left": None, "right": None},
+            "right": {"tag": "number", "value": 0, "left": None, "right": None},
+        },
+        "right": {"tag": "number", "value": 0, "left": None, "right": None},
+    }
+
+    tokens = tokenize(f"1 && 0 && 0")
+    ast = parse(tokens)
+    assert ast == {
+        "tag": "&&",
+        "value": None,
+        "left": {
+            "tag": "&&",
+            "value": None,
+            "left": {"tag": "number", "value": 1, "left": None, "right": None},
+            "right": {"tag": "number", "value": 0, "left": None, "right": None},
+        },
+        "right": {"tag": "number", "value": 0, "left": None, "right": None},
     }
 
 
@@ -279,11 +371,12 @@ def test_format_ast():
 
 
 if __name__ == "__main__":
-    # test_simple_addition_parsing()
-    # test_simple_identifier_parsing()
-    # test_unary_operators_parsing()
+    test_simple_addition_parsing()
+    test_simple_identifier_parsing()
+    test_unary_operators_parsing()
+    test_logical_operators_parsing()
     test_relational_operators_parsing()
-    # test_nested_expressions_parsing()
-    # test_operator_precedence_parsing()
-    # test_assignment_parsing()
+    test_nested_expressions_parsing()
+    test_operator_precedence_parsing()
+    test_assignment_parsing()
     print("done.")
